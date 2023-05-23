1701986675f0a502d804fbfc33886450de740cb0
JiminLeeDev <75084693+JiminLeeDev@users.noreply.github.com>
2023-05-23 18:37:04 +0900

    커밋

--- src/components/badge_list.js
+++ src/components/badge_list.js
@@ -4,7 +4,20 @@ function BadgeList({ contents, is_pc, is_tablet, is_mobile }) {
   return (
     <div className={styles.wrapper}>
       {contents.map((content) => (
-        <span className={styles.badge}>{content}</span>
+        <span
+          className={styles.badge}
+          style={{
+            fontSize: is_pc
+              ? "2rem"
+              : is_tablet
+              ? "1.5rem"
+              : is_mobile
+              ? "1rem"
+              : "0.5rem",
+          }}
+        >
+          {content}
+        </span>
       ))}
     </div>
   );
